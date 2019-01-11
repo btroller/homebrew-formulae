eaa4831949c6fe3a52e91f083c13424eab09b9c6
Benjamin Troller <7834154+btroller@users.noreply.github.com>
2019-01-10 19:40:05 -0800

    Added sha256 for verification purposes

--- otp.rb
+++ otp.rb
@@ -3,6 +3,7 @@ class Otp < Formula
   homepage "https://github.com/btroller/One-Time-Pad"
   url "https://github.com/btroller/One-Time-Pad/archive/2.0.tar.gz"
   version "2.0"
+  sha256 "51c746a729a7f980fad5803584337dcb99269bce148a28d76de0989fd0af9927"
 
   def install
     system "make"
