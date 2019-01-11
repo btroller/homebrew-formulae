f9090dd006c1dc21b2a94c254bd60d7f70cb722f
Benjamin Troller <7834154+btroller@users.noreply.github.com>
2019-01-10 19:34:47 -0800

    Create otp.rb

---
+++ otp.rb
@@ -0,0 +1,11 @@
+class OTP < Formula
+  desc "One-time pad encryption/decryption software -- NOT VERIFIED AS SECURE"
+  homepage "https://github.com/btroller/One-Time-Pad"
+  url "https://github.com/btroller/One-Time-Pad/archive/2.0.tar.gz"
+  version "2.0"
+
+  def install
+    system "make"
+    bin.install "otp"
+  end
+end
